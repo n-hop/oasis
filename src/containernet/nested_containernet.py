5addc12d6e06c96b2b5009857cd3d9f5d1d9fd55
Peng LEI <peng.lei@n-hop.com>
2024-07-29 08:51:59 +0800

    chore: cleanup the source

--- src/containernet/nested_containernet.py
+++ src/containernet/nested_containernet.py
@@ -26,7 +26,7 @@ def load_nested_config(nested_config_file: str, nested_containernet: str) -> Nes
     if nested_config_file == "" or nested_containernet == "":
         return None
     containernet_list = []
-    with open(nested_config_file, 'r') as stream:
+    with open(nested_config_file, 'r', encoding='utf-8') as stream:
         try:
             nested_config = yaml.safe_load(stream)
             containernet_list = nested_config["containernet"]
@@ -37,7 +37,7 @@ def load_nested_config(nested_config_file: str, nested_containernet: str) -> Nes
     for containernet in containernet_names:
         if containernet == nested_containernet:
             logging.info(
-                f"loaded containernet: {containernet_list[containernet]}")
+                f"loaded containernet: %s", {containernet_list[containernet]})
             return NestedConfig(**containernet_list[containernet])
     return None
 
@@ -48,22 +48,25 @@ class NestedContainernet():
         self.config = config
         self.workspace = workspace
         self.test_name = test_name
+        self.test_container_name = ""
         self.setUp()
 
     def setUp(self) -> None:
         logging.info(
-            "########################## Test Framework setup NestedContainernet ##########################")
+            "########################## Test Framework setup \
+            NestedContainernet##########################")
         self.formatted_mounts = self.get_formatted_mnt()
 
     def tearDown(self) -> None:
         logging.info(
-            f"NestedContainernet tearDown the Containernet.")
+            "NestedContainernet tearDown the Containernet.")
         # stop all the running containers with the name "nested_containernet**"
         os.system(
             "docker stop $(docker ps -a -q -fname=nested_containernet) || true")
         os.system("docker container prune --force || true")
         logging.info(
-            "########################## Test Framework teardown NestedContainernet ##########################")
+            "########################## Test Framework teardown \
+            NestedContainernet##########################")
 
     def stop(self):
         self.tearDown()
@@ -73,7 +76,7 @@ class NestedContainernet():
         self.test_container_name = "nested_containernet_" + \
             str(random.randint(0, 1000)) + "_" + self.test_name
         logging.info(
-            f"NestedContainernet start the Containernet.")
+            "NestedContainernet start the Containernet.")
         start_cmd = "docker run -d -t --rm "
         start_cmd += f" --name '{self.test_container_name}' "
         start_cmd += f" --hostname '{self.test_container_name}' "
@@ -90,34 +93,43 @@ class NestedContainernet():
         start_cmd += f" {self.formatted_mounts} "
         start_cmd += f" {self.config.image}"
         logging.info(
-            f"NestedContainernet start_cmd: {start_cmd}")
+            f"NestedContainernet start_cmd: %s", start_cmd)
         ret = os.system(start_cmd)
         return ret == 0
 
     def execute(self, cmd):
-        test_case_cmd = f"docker exec {self.test_container_name} /bin/bash -c \"{cmd}\""
-        clean_cmd = f"docker exec {self.test_container_name} /bin/bash -c \"mn --clean >/dev/null 2>&1\" || true"
+        test_case_cmd = f"docker exec {self.test_container_name} \
+            /bin/bash -c \"{cmd}\""
+        clean_cmd = f"docker exec {self.test_container_name} \
+            /bin/bash -c \"mn --clean >/dev/null 2>&1\" || true"
         logging.info(
-            "########################## Test Framework NestedContainernet Executing... ##########################")
+            "########################## Test Framework \
+            NestedContainernet Executing... ##########################")
         logging.info(
-            f"execute with \" {test_case_cmd} \"")
+            f"execute with \" %s \"", test_case_cmd)
         os.system(clean_cmd)
         os.system(test_case_cmd)
         os.system(clean_cmd)
 
     def get_formatted_mnt(self) -> str:
         logging.info(
-            f"NestedContainernet config mounts: {self.config.mounts}")
+            f"NestedContainernet config mounts: %s", self.config.mounts)
         if self.config.mounts is None:
             return ""
-        self.formatted_mounts = f" --mount type=bind,source={self.workspace},target=/root,bind-propagation=shared "
+        self.formatted_mounts = f" --mount \
+            type=bind,source={self.workspace},\
+            target=/root,bind-propagation=shared "
         for mount in self.config.mounts:
             source, target, *readonly = mount.split(":")
             if len(readonly) == 0:
-                mount_cmd = f"--mount type=bind,source={source},target={target},bind-propagation=shared "
+                mount_cmd = f"--mount \
+                    type=bind,source={source},\
+                    target={target},bind-propagation=shared "
             else:
-                mount_cmd = f"--mount type=bind,source={source},target={target},readonly,bind-propagation=shared "
+                mount_cmd = f"--mount \
+                    type=bind,source={source},\
+                    target={target},readonly,bind-propagation=shared "
             self.formatted_mounts += mount_cmd
         logging.info(
-            f"NestedContainernet formatted mounts: {self.formatted_mounts}")
+            f"NestedContainernet formatted mounts: %s", self.formatted_mounts)
         return self.formatted_mounts
--- src/run_test.py
+++ src/run_test.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import argparse
 import logging
 import yaml
 
@@ -8,49 +7,6 @@ from mininet.log import setLogLevel
 from containernet.linear_net import LinearNet
 from containernet.containernet import PairNet
 from containernet.node_config import NodeConfig
-from containernet.nested_containernet import (
-    NestedContainernet, NestedConfig, load_nested_config)
-
-
-def parse_args():
-    """
-    Parse command line arguments.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n',
-                        help='YAML configuration file for nested containernet',
-                        dest='nested_config_file',
-                        type=str,
-                        default="")
-    parser.add_argument('--containernet',
-                        help='nested containernet name in the YAML file',
-                        dest='nested_containernet',
-                        type=str,
-                        default="nuc_sz")
-    parser.add_argument('-t',
-                        help='YAML file for the test case to be executed',
-                        dest='tests_config_file',
-                        type=str,
-                        default="")
-    parser.add_argument('-w',
-                        help='workspace directory',
-                        dest='workspace',
-                        type=str,
-                        default="")
-    return parser
-
-
-def build_nested_env(nested_config_file, nested_containernet, workspace):
-    nested_config = load_nested_config(
-        nested_config_file, nested_containernet)
-    if nested_config is None:
-        logging.info(
-            f"Error: {nested_containernet} is not in the nested config file.")
-        sys.exit(1)
-    test_name = "default "
-    # execute the test cases on nested containernet
-    return NestedContainernet(
-        nested_config, workspace, test_name)
 
 
 def build_network(yaml_file_path):
@@ -71,20 +27,10 @@ if __name__ == '__main__':
     setLogLevel('info')
     logging.basicConfig(level=logging.INFO)
 
-    local_parser = parse_args()
-    ns, args = local_parser.parse_known_args()
-    cur_config_yaml_file_path = ns.tests_config_file
-    nested_config_file = ns.nested_config_file
-    nested_containernet = ns.nested_containernet
-    cur_workspace = ns.workspace
+    cur_workspace = sys.argv[1]
+    cur_config_yaml_file_path = sys.argv[2]
 
     if not os.path.exists(cur_config_yaml_file_path):
         logging.info(f"Error: %s does not exist.", cur_config_yaml_file_path)
         sys.exit(1)
-    nested_env = build_nested_env(
-        nested_config_file, nested_containernet, cur_workspace)
-    nested_env.start()
-    nested_env.execute("")
-    # self.docker_cmd = f"python3 execute_test.py {case_config_yaml_file_path} {self.workspace}"
-    # pair_net = build_network(cur_config_yaml_file_path)
-    nested_env.stop()
+    pair_net = build_network(cur_config_yaml_file_path)
