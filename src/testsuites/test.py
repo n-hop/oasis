604a2b005c2d5429535a555d6b60356f5a669243
Peng LEI <peng.lei@n-hop.com>
2024-08-04 20:59:01 +0800

    chore: refined ping test

--- src/testsuites/test.py
+++ src/testsuites/test.py
@@ -12,12 +12,29 @@ class TestType(IntEnum):
 
 @dataclass
 class TestConfig:
+    """
+    TestConfig is a dataclass that holds the configuration for the test.
+    interval: The interval time for the test.
+    interval_num: The number of intervals.
+    packet_size: The size of the packet to be sent.
+    packet_count: The number of packets to be sent.
+    log_file: The file to log the test results.
+    test_type: The type of test to be performed.
+    client_host: The host id of the client. 
+        for iperf, iperf client will be run on this host.
+        if None, iperf client will be run on all hosts.
+    server_host: The host id of the server.
+        for iperf, iperf server will be run on this host.
+        If None, iperf server will be run on all hosts.
+    """
     interval: Optional[float] = field(default=1.0)
     interval_num: Optional[int] = field(default=10)
     packet_size: Optional[int] = field(default=1024)
     packet_count: Optional[int] = field(default=10)
     log_file: Optional[str] = field(default=None)
     test_type: Optional[TestType] = field(default=TestType.throughput)
+    client_host: Optional[int] = field(default=None)
+    server_host: Optional[int] = field(default=None)
 
 
 class ITestSuite(ABC):
--- src/testsuites/test_ping.py
+++ src/testsuites/test_ping.py
@@ -15,15 +15,33 @@ class PingTest(ITestSuite):
     def _run_test(self, network: INetwork):
         hosts = network.get_hosts()
         hosts_num = len(hosts)
-        for i in range(hosts_num):
-            if i == 0:
-                continue
-            logging.info(
-                f"############### Oasis PingTest from "
-                "%s to %s ###############",hosts[i].name(), hosts[0].name())
-            res = hosts[i].cmd('ping -c 5 -W 1 -i 0.1 %s' % hosts[0].IP())
-            logging.info('host %s', res)
-            if "100% packet loss" in res:
-                logging.error("Ping test failed")
-                return False
+        if self.config.client_host is None or self.config.server_host is None:
+            for i in range(hosts_num):
+                if i == 0:
+                    continue
+                logging.info(
+                    f"############### Oasis PingTest from "
+                    "%s to %s ###############",hosts[i].name(), hosts[0].name())
+                res = hosts[i].cmd(f'ping -c 5 -W 1 -i 0.1 '
+                                   f'{hosts[0].IP()}'
+                                   f' > {self.config.log_file}')
+                logging.info('host %s', res)
+                if "100% packet loss" in res:
+                    logging.error("Ping test failed")
+                    return False
+            return True
+        # Run ping test from client to server
+        logging.info(
+            f"############### Oasis PingTest from "
+            "%s to %s ###############", 
+            hosts[self.config.client_host].name(),
+            hosts[self.config.server_host].name())
+        res = hosts[self.config.client_host].cmd(
+            f'ping -c 5 -W 1 -i 0.1 '
+            f'{hosts[self.config.server_host].IP()}'
+            f' > {self.config.log_file}')
+        logging.info('host %s', res)
+        if "100% packet loss" in res:
+            logging.error("Ping test failed")
+            return False
         return True
