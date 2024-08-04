1d4927fecba6574a1a9e929d71423dfedd9bbf02
Peng LEI <peng.lei@n-hop.com>
2024-08-04 19:49:47 +0800

    chore(pingtest): refined pingtest suite

--- src/testsuites/test_ping.py
+++ src/testsuites/test_ping.py
@@ -13,9 +13,17 @@ class PingTest(ITestSuite):
         return True
 
     def _run_test(self, network: INetwork):
-        logging.info(
-            "############### Oasis PingTest ###########")
         hosts = network.get_hosts()
-        for host in hosts:
-            res = host.cmd('ping -c 5 -W 1 -i 0.1 %s' % hosts[0].IP())
+        hosts_num = len(hosts)
+        for i in range(hosts_num):
+            if i == 0:
+                continue
+            logging.info(
+                f"############### Oasis PingTest from "
+                "%s to %s ###############",hosts[i].name(), hosts[0].name())
+            res = hosts[i].cmd('ping -c 5 -W 1 -i 0.1 %s' % hosts[0].IP())
             logging.info('host %s', res)
+            if "100% packet loss" in res:
+                logging.error("Ping test failed")
+                return False
+        return True
