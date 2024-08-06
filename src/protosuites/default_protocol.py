5967339aef9d134592e861ee742ca7230f4bb68b
Peng LEI <peng.lei@n-hop.com>
2024-08-06 21:13:42 +0800

    chore: supported protocols

---
+++ src/protosuites/default_protocol.py
@@ -0,0 +1,15 @@
+from interfaces.network import INetwork
+from .proto import IProtoSuite
+
+class DefaultProtocol(IProtoSuite):
+    def post_run(self):
+        return True
+
+    def pre_run(self):
+        return True
+
+    def run(self, network: INetwork):
+        return True
+
+    def stop(self, network: INetwork):
+        return True
---
+++ src/protosuites/kcp_protocol.py
@@ -0,0 +1,16 @@
+# import logging
+from interfaces.network import INetwork
+from .proto import IProtoSuite
+
+class KCPProtocol(IProtoSuite):
+    def post_run(self):
+        return True
+
+    def pre_run(self):
+        return True
+
+    def run(self, network: INetwork):
+        return True
+
+    def stop(self, network: INetwork):
+        return True
