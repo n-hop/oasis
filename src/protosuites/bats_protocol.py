ee282eaae92fe2ba6135791c264cfdbea3a46400
Peng LEI <peng.lei@n-hop.com>
2024-08-07 19:13:25 +0800

    chore: install licence for bats

--- src/protosuites/bats_protocol.py
+++ src/protosuites/bats_protocol.py
@@ -2,11 +2,31 @@ import logging
 from interfaces.network import INetwork
 from .proto import IProtoSuite
 
+
 class BATSProtocol(IProtoSuite):
-    def post_run(self):
+    def post_run(self, network: INetwork):
         return True
 
-    def pre_run(self):
+    def pre_run(self, network: INetwork):
+        # Init the license file for bats, otherwise it will not run.
+        hosts = network.get_hosts()
+        if self.config.protocol_path is None or self.config.hosts is None:
+            logging.error("No protocol path or hosts set")
+            return False
+        # get path from `self.config.protocol_path`
+        prefix_path = '/'.join(self.config.protocol_path.split('/')[:-1])
+        if prefix_path == '':
+            prefix_path = '.'
+        host_num = len(hosts)
+        for i in range(host_num):
+            hosts[i].cmd(
+                f'mkdir -p /etc/cfg')
+            hosts[i].cmd(
+                f'cp {prefix_path}/licence /etc/cfg/')
+            logging.info(
+                f"############### Oasis install licence file on "
+                "%s ###############",
+                hosts[i].name())
         return True
 
     def run(self, network: INetwork):
@@ -18,11 +38,11 @@ class BATSProtocol(IProtoSuite):
         for i in range(host_num):
             res = hosts[i].cmd(
                 f'{self.config.protocol_path} {self.config.protocol_args} '
-                f' > {self.config.log_file}')
+                f' ')
             logging.info(
                 f"############### Oasis run bats protocol on "
-                "%s, %s ###############", 
-                hosts[i].name(),res)
+                "%s, %s ###############",
+                hosts[i].name(), res)
         return True
 
     def stop(self, network: INetwork):
@@ -35,7 +55,7 @@ class BATSProtocol(IProtoSuite):
         for i in range(host_num):
             logging.info(
                 f"############### Oasis stop bats protocol on "
-                "%s ###############", 
+                "%s ###############",
                 hosts[i].name())
             hosts[i].cmd(
                 f'pkill -f {process_name}')
--- src/protosuites/default_protocol.py
+++ src/protosuites/default_protocol.py
@@ -1,11 +1,12 @@
 from interfaces.network import INetwork
 from .proto import IProtoSuite
 
+
 class DefaultProtocol(IProtoSuite):
-    def post_run(self):
+    def post_run(self, network: INetwork):
         return True
 
-    def pre_run(self):
+    def pre_run(self, network: INetwork):
         return True
 
     def run(self, network: INetwork):
