cd68dba18e71f786930ac4c752a38072ed30f5da
Peng LEI <peng.lei@n-hop.com>
2024-08-12 15:10:39 +0800

    chore: add comments to proto info

--- src/protosuites/proto_info.py
+++ src/protosuites/proto_info.py
@@ -2,10 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class IProtoInfo(ABC):
+    """For a classic proxy protocol, it should have defined at least one of the following IPC mechanisms:
+    - UDP forward port
+    - TUN interface
+    - UDS listening port
+    """
     @abstractmethod
     def get_forward_port(self) -> int:
-        pass
+        """The UDP forward port of the protocol.
+        """
 
     @abstractmethod
     def get_tun_ip(self) -> str:
-        pass
+        """The ip address of the tun interface.
+
+        Returns:
+            str: ip address of the tun interface
+        """
