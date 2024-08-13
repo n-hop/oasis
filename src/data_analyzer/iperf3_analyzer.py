0be839a74604e2d100bcf58432141f1a666dc86f
eric.cai <eric.cai@n-hop.com>
2024-08-13 15:50:27 +0800

    chore: analyze iperf3 result

--- src/data_analyzer/iperf3_analyzer.py
+++ src/data_analyzer/iperf3_analyzer.py
@@ -13,7 +13,17 @@ class Iperf3Analyzer(IDataAnalyzer):
             logging.info(f"Analyze iperf3 log: %s", input_log)
             # dump file content
             with open(input_log, "r", encoding="utf-8") as f:
-                logging.info(f.read())
+                zero_bit_line_cnt = 0
+                lines = f.readlines()
+                for line in lines:
+                    zero_bit_line = r'0.00 Bytes  0.00 bits/sec'
+                    if zero_bit_line in line:
+                        zero_bit_line_cnt += 1
+                    else:
+                        zero_bit_line_cnt = 0
+                    if zero_bit_line_cnt > 3:
+                        logging.info("The iperf3 %s has too many zero bit", input_log)
+                        break
 
     def visualize(self):
         # Implement visualization logic for iperf3 logs
