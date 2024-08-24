5759ddddeb987f96b5ebbda1321b71a1535c79c9
Peng LEI <peng.lei@n-hop.com>
2024-08-24 21:04:54 +0800

    chore: ping test param init

--- src/testsuites/test_ping.py
+++ src/testsuites/test_ping.py
@@ -22,7 +22,7 @@ class PingTest(ITestSuite):
                 logging.info(
                     f"############### Oasis PingTest from "
                     "%s to %s ###############", hosts[i].name(), hosts[0].name())
-                res = hosts[i].cmd(f'ping -c 5 -W 1 -i 0.1 '
+                res = hosts[i].cmd(f'ping -W 1 -c {self.config.interval_num} -i {self.config.interval} '
                                    f'{hosts[0].IP()}'
                                    f' > {self.result.record}')
                 logging.info('host %s', res)
@@ -37,7 +37,7 @@ class PingTest(ITestSuite):
             hosts[self.config.client_host].name(),
             hosts[self.config.server_host].name())
         res = hosts[self.config.client_host].popen(
-            f'ping -c 100 -W 1 -i 0.01 '
+            f'ping -W 1 -c {self.config.interval_num} -i {self.config.interval} '
             f'{hosts[self.config.server_host].IP()}').stdout.read().decode('utf-8')
         logging.info('host %s', res)
         if "100% packet loss" in res:
